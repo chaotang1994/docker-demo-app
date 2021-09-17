d66904227916ce69f9eb1c7bd012fe6608024663
Chaoxin Tang <chaotang1994@gmail.com>
2021-09-17 15:26:47 -0400

    new commit

---
+++ src/main/java/com/ex/dockerspringboothello/api/ProductController.java
@@ -0,0 +1,37 @@
+package com.ex.dockerspringboothello.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ex.dockerspringboothello.model.Product;
+
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+	static Map<String, Product> productMap = new HashMap<>();
+	
+	static {
+		productMap.put("P1001", new Product("P1001", "Mobile",123));
+		productMap.put("P1002", new Product("P1002", "Watch",32));
+		productMap.put("P1003", new Product("P1003", "Laptop",756));
+		productMap.put("P1004", new Product("P1004", "Charger",32));
+	}
+	
+	
+
+	@GetMapping(value = "/{productId}")
+	public Product getProductDetail(@PathVariable String productId) {
+		
+		Product product = productMap.get(productId);
+		System.out.println("Product Detail : "+product);
+		return product;
+	}
+	
+}
